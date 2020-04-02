477a999d32cafd122abe1b69a13aa661477d66e7
Joshua Perez Leduc <joshuapleduc@gmail.com>
2020-04-02 17:17:18 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,7 +1,8 @@
 def prime?(int)
   false if int.even? && int == 1
-  factors = (3..max)
-  factors.each do |i|
-      false if divisible_by? i
-    end
+  false if int % 10 == 5
+  # factors = (3..200)
+  # factors.each do |i|
+  #     false if divisible_by? i
+  #   end
 end
