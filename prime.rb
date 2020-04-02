2c08810b170c48078bdf8a2af8257e339d39e5fe
Joshua Perez Leduc <joshuapleduc@gmail.com>
2020-04-02 17:23:23 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,20 +1,3 @@
-require 'pry'
-
 def prime?(int)
-  if int == 2
-    return true
-  elsif int.even? && int == 1 || 0
-    return false
-    binding.pry
-  elsif int % 10 == 5
-    return false
-  elsif int.negative?
-    return false
-  else
-    return true
-  end
-  # factors = (3..200)
-  # factors.each do |i|
-  #     false if divisible_by? i
-  #   end
+
 end
