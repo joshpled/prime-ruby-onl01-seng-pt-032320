8e636fe42a4e864637d1daf3e45a4792100dc23a
Joshua Perez Leduc <joshuapleduc@gmail.com>
2020-04-02 17:20:56 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,12 +1,12 @@
 def prime?(int)
-  if int.even? && int == 1 || 0
+  if int == 2
+    return true
+    elsif int.even? && int == 1 || 0
     return false
   elsif int % 10 == 5
     return false
   elsif int.negative?
     return false
-  elsif int == 2
-    return true
   else
     return true
   end
